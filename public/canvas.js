46d8ac15e3e6ccc38717aa3004b3c74a7fb04c94
Lee <lee1331@hotmail.co.uk>
2018-12-15 13:55:32 +0000

    Adding html buttons, changing button/tab styling, adding random peer brush colours

--- public/canvas.js
+++ public/canvas.js
@@ -3,15 +3,22 @@
         let canvas = createCanvas(window.innerWidth / 1.33 , window.innerHeight / 1.33);
         //move the canvas into the 'canvas' div
         canvas.parent('canvas');
-        
         background(0);
         socket = io.connect('http://localhost:5500');
+
+        //let red = fill(244,67,54);
+        //let green = fill(76,175,80);
+        //let blue = fill(0,140,186);
+        let r = random(255);
+        let g = random(255);
+        let b = random(255);
 
         //if this socket recives a message called 'mouse'
         socket.on('mouse', function(data){
             //draw AT the location of the data object -  data.x and y
             noStroke();
-            fill(0, 0, 100);
+            fill(r, g, b);
+            //fill(0, 0, 100);
             ellipse(data.x, data.y, 36, 36);
         });
         
@@ -20,13 +27,14 @@
 
 
     //'mousedDragged' is a reserved p5 name
-        //only triggers when the mouse is dragged (and held down i think)
+        //only triggers when the mouse is dragged and held down     
     function mouseDragged(){
         
         //removes outline from brush
         noStroke();
 
         fill(255);
+        //fill(r, g, b);
 
         //draw an ellipse
         ellipse(mouseX, mouseY, 36, 36);
