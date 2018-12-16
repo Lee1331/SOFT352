86a4f1822a7023e0045c5881856303f33cae8d90
Lee <lee1331@hotmail.co.uk>
2018-12-16 14:58:52 +0000

    UI Fixes/Improvements

--- public/canvas.js
+++ public/canvas.js
@@ -1,51 +1,62 @@
-    function setup(){
-        //let canvas = createCanvas(200,200);
-        let canvas = createCanvas(window.innerWidth / 1.33 , window.innerHeight / 1.33);
-        //move the canvas into the 'canvas' div
-        canvas.parent('canvas');
-        background(0);
-        socket = io.connect('http://localhost:5500');
+let r, g, b;
+let canvasDiv = document.getElementById('canvas');
+let canvasWidth = canvasDiv.offsetWidth; 
+let canvasHeight = canvasDiv.offsetHeight; 
 
-        //let red = fill(244,67,54);
-        //let green = fill(76,175,80);
-        //let blue = fill(0,140,186);
-        let r = random(255);
-        let g = random(255);
-        let b = random(255);
+function setup(){
 
-        //if this socket recives a message called 'mouse'
-        socket.on('mouse', function(data){
-            //draw AT the location of the data object -  data.x and y
-            noStroke();
-            fill(r, g, b);
-            //fill(0, 0, 100);
-            ellipse(data.x, data.y, 36, 36);
-        });
-        
-    }
+    let canvas = createCanvas(canvasWidth, canvasHeight); 
 
+    //move the canvas into the 'canvas' div
+    canvas.parent('canvas');
+    background(0);
+    socket = io.connect('http://localhost:5500');
 
+    //test -  assign other users brush colour randomly
+    r = random(255);
+    g = random(255);
+    b = random(255);
 
-    //'mousedDragged' is a reserved p5 name
-        //only triggers when the mouse is dragged and held down     
-    function mouseDragged(){
-        
-        //removes outline from brush
+    //if this socket recives a message called 'mouse'
+    socket.on('mouse', function(data){
+        //draw AT the location of the data object -  data.x and y
         noStroke();
+        fill(r, g, b);
+        //fill(0, 0, 100);
+        ellipse(data.x, data.y, 36, 36);
+    });
+    
+}
 
-        fill(255);
-        //fill(r, g, b);
+//'mousedDragged' is a reserved p5 name
+    //only triggers when the mouse is dragged and held down     
+function mouseDragged(){
+    
+    //removes outline from brush
+    noStroke();
 
-        //draw an ellipse
-        ellipse(mouseX, mouseY, 36, 36);
+    fill(255);
 
-        //SENDING DATA ON THE CLIENT END
-        //when we want to send a message, we create a js object which contains the data we want to send
-        let data = {
-            x: mouseX,
-            y: mouseY
-        }
-        //then we emit the data, and inlcude a name for the message ('mouse' in this case)
-        socket.emit('mouse', data);
-        //console.log(`Sending : ${mouseX}, ${mouseY} from socket ${socket.id}`);
+    //draw an ellipse
+    ellipse(mouseX, mouseY, 36, 36);
+
+    //SENDING DATA ON THE CLIENT END
+    //when we want to send a message, we create a js object which contains the data we want to send
+    let data = {
+        x: mouseX,
+        y: mouseY
     }
+    //then we emit the data, and inlcude a name for the message ('mouse' in this case)
+    socket.emit('mouse', data);
+    //console.log(`Sending : ${mouseX}, ${mouseY} from socket ${socket.id}`);
+}
+
+function mousePressed(){
+    ellipse(mouseX, mouseY, 36, 36);
+    //fill(r, g, b);
+}
+
+function windowResized() {
+    //resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(canvasWidth, canvasHeight);
+}
